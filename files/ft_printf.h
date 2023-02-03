6f5b860454331007d3df9244a1f0643436151017
Chukyy <51385693+Chukyy712@users.noreply.github.com>
2023-02-03 14:53:20 +0000

    Fversion

--- files/ft_printf.h
+++ files/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: brcaetan <brcaetan@42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 14:38:52 by brcaetan          #+#    #+#             */
-/*   Updated: 2023/01/28 02:15:41 by brcaetan         ###   ########.fr       */
+/*   Updated: 2023/01/30 20:59:50 by brcaetan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define FT_PRINTF_H
 # include <stdarg.h>
 # include <unistd.h>
-
 
 int	ft_printf(const char *src, ...);
 
